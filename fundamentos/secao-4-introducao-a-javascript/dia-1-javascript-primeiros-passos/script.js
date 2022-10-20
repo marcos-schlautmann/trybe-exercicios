7a130eccc530bf14b12dddbd508293f30b938360
Marcos <marcos27.bonfim@gmail.com>
2022-10-20 16:24:16 -0300

    Exercicios de variaveis

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,12 @@
+let age = 20;
+let nome = "Hulk";
+const myName = 'Marcos';
+const birthCity = 'São Caetano do Sul';
+let birthYear = '2030';
+
+
+console.log(age);
+console.log(nome);
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
