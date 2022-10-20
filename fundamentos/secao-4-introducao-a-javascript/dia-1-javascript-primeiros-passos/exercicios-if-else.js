c27f1ff481af8a40c8bd9de6e29bf2614755d5c8
Marcos <marcos27.bonfim@gmail.com>
2022-10-20 19:17:47 -0300

    exercicios operadores e if/else

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-if-else.js
@@ -0,0 +1,38 @@
+/* Programa retorna maior numero  */
+
+let num1 = 8;
+let num2 = 16;
+let num3 = 15;
+
+// if (num1 > num2) {
+//     console.log(num1);
+// }
+// else {
+//     console.log(num2);
+// }
+
+/* Programa retorna maior de 3 numeros */
+
+// if (num1 > num2 && num1 > num3) {
+//     console.log(num1);
+// }
+// else if (num2 > num1 && num2 > num3) {
+//     console.log(num2);
+// }
+// else {
+//     console.log(num3);
+// }
+
+/* Programa retorna positive e negative */
+
+// if (num1 > 0) {
+//     console.log('O número digitado é positive !');
+// } 
+// else if(num1 < 0) {
+//     console.log('O número digitado é negative !');
+// }
+// else {
+//     console.log('O número digitado é igual a zero !');
+// }
+
+/* Programa retorna angulos do triângulo */
---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-operadores.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b = 5;
+let soma;
+let sub;
+let mult;
+let div;
+let mod;
+
+soma = a + b;
+sub = a - b;
+mult = a * b;
+div = a / b;
+mod = a % b;
+
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(mod);
