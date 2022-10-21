f69a4c5693a5418a60c8bc4e86cab5af13f20fee
Marcos <marcos27.bonfim@gmail.com>
2022-10-21 19:38:09 -0300

    exercicios lidando com arrays

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-dia-2.js
@@ -0,0 +1,62 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let numbers = [5, 9, 3, 2, 7, 8, 10, 2, 5, 2];
+
+// Exibe todos os valores
+
+// for (i = 0; i < numbers.length; i += 1) {
+//     console.log(numbers[i]);
+// }
+
+/* Soma todos os valores */
+
+// let soma = 0;
+// for (i = 0; i < numbers.length; i += 1) {
+//     soma = soma + numbers[i];
+// }
+// console.log(soma);
+
+// Calcula e imprime a média 
+
+// console.log(numbers.length);
+
+// let soma = 0;
+// let media = 0;
+// for (i = 0; i < numbers.length; i += 1) {
+//     soma = soma + numbers[i];
+// }
+// media = soma / numbers.length;
+// console.log(media);
+
+
+// Imprimi mensagem maior que 20 ou menor que 20
+
+// let soma = 0;
+// let media = 0;
+// for (i = 0; i < numbers.length; i += 1) {
+//     soma = soma + numbers[i];
+//     media = soma / numbers.length;
+// }
+// if (media > 20) {
+//     console.log('O valor é maior que 20 !');
+// }
+// else {
+//     console.log('O valor é menor que 20 !');
+// }
+// console.log(media);
+
+
+// Descobre o maior valor do array e imprime
+
+// let maiorNumero = 0;
+// let menorNumero = 0;
+// for (i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > maiorNumero) {
+//         maiorNumero = numbers[i];
+//     }
+// }
+// console.log(maiorNumero);
+
+
+
+
