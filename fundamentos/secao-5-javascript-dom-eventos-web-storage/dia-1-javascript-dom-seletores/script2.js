137be01c025e5ea360bf6647e618b6f083dc1a3c
Marcos <marcos27.bonfim@gmail.com>
2022-10-31 22:07:49 -0300

    Exercicios exemplos de seletores

---
+++ fundamentos/secao-5-javascript-dom-eventos-web-storage/dia-1-javascript-dom-seletores/script2.js
@@ -0,0 +1,32 @@
+// Script do exercicio exemplo 1
+
+// const paragraph = document.getElementById("paragraph");
+// paragraph.style.color = "red";
+
+// const tituloDaPagina = document.getElementById('page-title').innerText = 'Codigo de Conduta';
+
+// const secondParagraph = document.getElementById('second-paragraph').innerText = 'O filme Código de Conduta traz a história de um homem chamado Clayd Shelton que busca vingar a morte de sua mulher e filha, mortas por um traficante viciado que saiu pela porta da frente da cadeia após fazer um acordo com a polícia';
+
+// document.getElementById('second-paragraph').style.color = 'blue';
+
+// const subtitle = document.getElementById('subtitle').style.backgroundColor = 'red';
+
+// document.getElementById('subtitle').style.color = 'white';
+
+
+// Script do exercicio 2
+
+// const paragraph = document.getElementById("paragraph");
+// paragraph.style.color = "black";
+// paragraph.style.backgroundColor = 'gray';
+
+const para = document.getElementsByClassName('para');
+document.getElementsByClassName('para')[0].style.color = 'blue';
+document.getElementsByClassName('para')[0].style.fontSize = '100px'
+
+const subtitle = document.getElementsByTagName('h4')[0].style.color = 'green';
+
+
+
+
+
