daa182b240d4c1b34aefee1016b29c1b101c185f
Marcos <marcos27.bonfim@gmail.com>
2022-11-21 19:39:41 -0300

    finalizando exercicio 2

--- fundamentos/secao-7-es6-testes-unitarios/dia-2-primeiros-passos-em-jest/exercicios/exercicio-2.test.js
+++ fundamentos/secao-7-es6-testes-unitarios/dia-2-primeiros-passos-em-jest/exercicios/exercicio-2.test.js
@@ -1,12 +1,12 @@
-const { describe } = require('yargs');
 const myFizzBuzz = require('./exercicio-2.js');
 
-describe('teste da função myFizzBuzz')
+describe('Testando a função myFizzBuzz', () => {
 
-
-
-
-
-
-
-
+    it('recebe um numero e retorna uma valor de acordo com a entrada', () => {
+        expect(myFizzBuzz(15)).toBe('fizzbuzz');
+        expect(myFizzBuzz(3)).toBe('fizz');
+        expect(myFizzBuzz(5)).toBe('buzz');
+        expect(myFizzBuzz(7)).toBe(7);
+        expect(myFizzBuzz('15')).toBe(false);
+    });
+});
