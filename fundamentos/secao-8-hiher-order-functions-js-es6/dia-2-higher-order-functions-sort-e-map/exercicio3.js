a98c6cc0c7658299b17ef4ae702b8e8d29fceb8e
Marcos <marcos27.bonfim@gmail.com>
2022-11-30 18:41:18 -0300

    exercicios dia 2 e finalizando exercicio 1

---
+++ fundamentos/secao-8-hiher-order-functions-js-es6/dia-2-higher-order-functions-sort-e-map/exercicio3.js
@@ -0,0 +1,12 @@
+const expectedResult = [
+    'George R. R. Martin - 1948',
+    'J. R. R. Tolkien - 1892',
+    'Isaac Asimov - 1920',
+    'Frank Herbert - 1920',
+    'Stephen King - 1947',
+    'H. P. Lovecraft - 1890',
+  ];
+  
+  const formatedAuthorNamesBirth = () => {
+    // escreva seu código aqui
+  }
