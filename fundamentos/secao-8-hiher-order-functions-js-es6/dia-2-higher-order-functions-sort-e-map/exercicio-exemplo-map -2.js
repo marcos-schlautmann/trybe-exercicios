de66ebc236006e4a871818327cfb165a5d729613
Marcos <marcos27.bonfim@gmail.com>
2022-11-30 18:04:43 -0300

    exercicios exemplos da secao 8 dia 2

---
+++ fundamentos/secao-8-hiher-order-functions-js-es6/dia-2-higher-order-functions-sort-e-map/exercicio-exemplo-map -2.js
@@ -0,0 +1,24 @@
+// utilizando map 
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+// utilizando for
+
+// const numbers = [1, 2, 3, 4, -5];
+
+// const negativeNumbers = [];
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] > 0) {
+//     negativeNumbers.push(numbers[index] * -1);
+//   } else {
+//     negativeNumbers.push(numbers[index]);
+//   }
+// }
+
+// console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+// console.log(numbers); // [ 1, 2, 3, 4, -5 ]
