0f9afed5f1f9c4dbf8092efb6312fc5dae0ee859
Marcos <marcos27.bonfim@gmail.com>
2023-01-10 16:30:05 -0300

    gerador de senha exemplo

--- front-end/secao-1-introducao-ao-front-end-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/GERADOR-DE-SENHAS/src/main.js
+++ front-end/secao-1-introducao-ao-front-end-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/GERADOR-DE-SENHAS/src/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+import { nanoid } from "nanoid";
+import './style.css';
+
+const passwordBtnElement = document.querySelector('button');
+const displayPasswordElement = document.querySelector('h2');
+
+passwordBtnElement.addEventListener('click', () => {
+    const randoPassword = nanoid();
+    displayPasswordElement.innerHTML = randoPassword;
+})
+
