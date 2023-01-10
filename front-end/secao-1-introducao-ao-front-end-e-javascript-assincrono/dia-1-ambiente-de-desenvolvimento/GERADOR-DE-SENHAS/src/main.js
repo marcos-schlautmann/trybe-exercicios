3e10ccdeaf6d42394007da77c21adf63d24d45fe
Marcos <marcos27.bonfim@gmail.com>
2023-01-10 18:26:26 -0300

    exemplo 2, copiando senha pra area de transferencia

--- front-end/secao-1-introducao-ao-front-end-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/GERADOR-DE-SENHAS/src/main.js
+++ front-end/secao-1-introducao-ao-front-end-e-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/GERADOR-DE-SENHAS/src/main.js
@@ -1,4 +1,5 @@
-import { nanoid } from "nanoid";
+import nanoid from 'nanoid';
+import copy from 'clipboard-copy';
 import './style.css';
 
 const passwordBtnElement = document.querySelector('button');
@@ -7,5 +8,10 @@ const displayPasswordElement = document.querySelector('h2');
 passwordBtnElement.addEventListener('click', () => {
     const randoPassword = nanoid();
     displayPasswordElement.innerHTML = randoPassword;
-})
+});
+
+displayPasswordElement.addEventListener('click', () => {
+    copy(event.target.innerHTML);
+    alert('Senha copiada com sucesso!');
+});
 
