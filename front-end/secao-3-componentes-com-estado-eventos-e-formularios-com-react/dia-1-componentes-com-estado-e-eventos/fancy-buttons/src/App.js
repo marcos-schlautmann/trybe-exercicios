a276f6ce4a1cfc092ff6f2edd5a33d099452026c
Marcos <marcos27.bonfim@gmail.com>
2023-01-25 15:39:17 -0300

    exercicios de fixação sobre estados e eventos juntos

--- front-end/secao-3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/secao-3-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import './App.css';
 
-function handleClick () {
-  console.log('Clicou no botão 1!');
-}
-
-function handleClick2 () {
-  console.log('Clicou no botão 2!');
-}
-
-function handleClick3 () {
-  console.log('Clicou no botão 3!');
-}
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);  
+    this.state = {
+      botao1 : 0,
+      botao2 : 0,
+      botao3 : 0,
+    }  
+  }
+
+  handleClick1 () {
+    this.setState((estadoAnteriorDoBotao1, _props) => ({
+      botao1: estadoAnteriorDoBotao1.botao1 + 1
+    }))
+  }
+  
+  handleClick2 () {
+    this.setState((estadoAnteriorDoBotao2, _props) => ({
+      botao2: estadoAnteriorDoBotao2.botao2 + 1
+    }))
+  }
+  
+ handleClick3 () {
+    this.setState((estadoAnteriorDoBotao3, _props) => ({
+      botao3: estadoAnteriorDoBotao3.botao3 + 1
+    }))
+  }
+
   render() {
     return(
      <div>
-      <button onClick={handleClick} >Botão 1!</button>
-      <button onClick={handleClick2} >Botão 2!</button>
-      <button onClick={handleClick3} >Botão 3</button>
+      <button onClick={this.handleClick1}>{this.state.botao1}</button>
+      <button onClick={this.handleClick2}>{this.state.botao2}</button>
+      <button onClick={this.handleClick3}>{this.state.botao3}</button>
      </div>
     )
   }
