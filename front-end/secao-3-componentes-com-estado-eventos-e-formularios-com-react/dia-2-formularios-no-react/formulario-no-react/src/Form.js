4b16fefed3851712f3f79d1d1d8f89db316c4a9e
Marcos <marcos27.bonfim@gmail.com>
2023-01-26 14:50:26 -0300

    exemplos de forms em react

---
+++ front-end/secao-3-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/formulario-no-react/src/Form.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Form extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>EStados em React: ferramenta incrível ou reagindo a regionalismos?</h1>
+                <form>
+                    
+                </form>
+            </div>
+        )
+    }
+}
+
+
+
+export default Form;
